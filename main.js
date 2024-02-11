d2208f897a8010bccfdd9b654f7dd27c3e5f34f1
VasRoko <30204032+VasRoko@users.noreply.github.com>
2024-02-11 15:12:22 +0000

    Split refactor

--- main.js
+++ main.js
@@ -1,29 +1,75 @@
-import invoice from './data/invoice.json' assert { type: "json" }; // assert not support on most browsers
+import invoiceData from './data/invoice.json' assert { type: "json" }; // assert not support on most browsers
+import playsData from './data/plays.json' assert { type: "json" }; // assert not support on most browsers
 
-// helpers
-import { volumeCreditsFor, amountFor, playFor } from './mainHelpers.js';
-
-const statement = (invoice) => {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format;
-
-    for (let perf of invoice.performances) {
-        // add volume credits 
-        volumeCredits += volumeCreditsFor(perf);
-
-        // print line for this order
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats) \n`;
-        totalAmount += amountFor(perf);
+const renderPlainText = (data, invoice, plays) => {
+    const playFor = (perf) => {
+        return plays[perf.playID];
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    
+    const amountFor = (perf) => {
+        let result = 0;
+        switch (playFor(perf).type) {
+            case "tragedy": 
+            result = 40000;
+            if (perf.audience > 30) {
+                result += 1000 * (perf.audience - 30);
+            } 
+            break;
+            case "comedy": 
+            result = 30000;
+            if (perf.audience > 20) {
+                    result += 10000 + 500 * (perf.audience - 20);                    
+                } 
+                result += 300 * perf.audience;
+            break;
+            default: 
+                throw new Exception(`unknow type: ${playFor(perf).type}`);
+            }
+            return result;
+    }
+        
+    const usd = (value) => {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }).format(value/100);
+    }
+    
+    const volumeCreditsFor = (perf) => {
+        let result = 0;
+        result += Math.max(perf.audience - 30, 0);
+        if ("comedy" == playFor(perf).type) result += Math.floor(perf.audience / 5);
+        return result;
+    }
+    
+    const totalVolumeCredits = () => {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+    
+    const totalAmount = () => {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
+
+    let result = `Statement for ${invoice.customer}\n`;
+    for (let perf of invoice.performances) {
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats) \n`;
+    }
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 }
 
-console.log(statement(invoice));
+const statement = (invoice, plays) => {
+    const statementData = {};
+    return renderPlainText(statementData, invoice, plays);
+}
+console.log(statement(invoiceData, playsData));
